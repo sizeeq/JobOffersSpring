0e2b4e46659eab499d200a1f63bcb14b1ecb94da
BuildTools <unconfigured@null.spigotmc.org>
2024-04-02 10:07:54 +0200

    added packages and facade classes

---
+++ src/test/java/pl/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,14 @@
+package pl.joboffers.domain.loginandregister;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginAndRegisterFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+
+}
---
+++ src/test/java/pl/joboffers/domain/offer/OfferFacadeTest.java
@@ -0,0 +1,13 @@
+package pl.joboffers.domain.offer;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OfferFacadeTest {
+
+    @Test
+    public void f() {
+
+    }
+}
