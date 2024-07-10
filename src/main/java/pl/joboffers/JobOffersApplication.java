afa4de6550f7bf40e171673100c94a03ff506461
BuildTools <unconfigured@null.spigotmc.org>
2024-07-10 11:41:36 +0200

    added spring boot and foundation for integration tests

--- src/main/java/pl/joboffers/JobOffersApplication.java
+++ src/main/java/pl/joboffers/JobOffersApplication.java
@@ -1,8 +1,12 @@
 package pl.joboffers;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JobOffersApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(JobOffersApplication.class, args);
     }
 }
