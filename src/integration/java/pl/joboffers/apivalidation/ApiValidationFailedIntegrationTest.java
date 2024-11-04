d9d0f9c04ba368cb4d72d1bcfc9941b47251dd05
BuildTools <devcodedred@gmail.com>
2024-11-04 18:20:04 +0100

    fixes

---
+++ src/integration/java/pl/joboffers/apivalidation/ApiValidationFailedIntegrationTest.java
@@ -0,0 +1,77 @@
+package pl.joboffers.apivalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import pl.joboffers.BaseIntegrationTest;
+import pl.joboffers.infrastructure.apivalidation.ApiValidationErrorDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ApiValidationFailedIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void should_return_400_bad_request_validation_message_when_request_has_blank_input() throws Exception {
+        //given
+        //when
+        ResultActions performPost = mockMvc.perform(post("/offers")
+                .content(
+                        """
+                                {
+                                "companyName": "",
+                                "position": "Java Developer",
+                                "salary": 6000,
+                                "offerUrl": "https://nofluffjobs.com/pl/job/mid-java-developer-stackmine-poznan-1"
+                                }
+                                """)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        MvcResult mvcResult = performPost.andExpect(status().isBadRequest()).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+        assertThat(result.messages()).containsExactly("companyName cannot be blank");
+    }
+
+//    public void should_return_409_conflict_when_there_is_already_added_offer_with_exact_same_url() throws Exception {
+//        //given
+//        //when
+//        ResultActions performPost = mockMvc.perform(post("/offers")
+//                .content(
+//                        """
+//                                {
+//                                "companyName": "Javappa",
+//                                "position": "Java Developer",
+//                                "salary": 6000,
+//                                "offerUrl": "https://nofluffjobs.com/pl/job/mid-java-developer-stackmine-poznan-1"
+//                                }
+//                                """)
+//                .contentType(MediaType.APPLICATION_JSON)
+//        );
+//
+//        ResultActions performPost2 = mockMvc.perform(post("/offers")
+//                .content(
+//                        """
+//                                {
+//                                "companyName": "Javappa",
+//                                "position": "Java Developer",
+//                                "salary": 6000,
+//                                "offerUrl": "https://nofluffjobs.com/pl/job/mid-java-developer-stackmine-poznan-1"
+//                                }
+//                                """)
+//                .contentType(MediaType.APPLICATION_JSON)
+//        );
+//
+//        //then
+//        performPost2.andExpect(status().isConflict())
+//                .andExpect(content().json(
+//                        """
+//                                """.trim()
+//                ));
+//    }
+}
--- src/main/java/pl/joboffers/infrastructure/offer/controller/OfferController.java
+++ src/main/java/pl/joboffers/infrastructure/offer/controller/OfferController.java
@@ -1,12 +1,13 @@
 package pl.joboffers.infrastructure.offer.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.joboffers.domain.offer.OfferFacade;
+import pl.joboffers.domain.offer.dto.OfferRequestDto;
 import pl.joboffers.domain.offer.dto.OfferResponseDto;
 
 import java.util.List;
@@ -30,5 +31,12 @@ public class OfferController {
         log.info("Fetching offer with id {}", id);
         OfferResponseDto offerById = offerFacade.findOfferById(id);
         return ResponseEntity.ok(offerById);
+    }
+
+    @PostMapping("/offers")
+    public ResponseEntity<OfferResponseDto> addOffer(@RequestBody @Valid final OfferRequestDto offerRequestDto) {
+        log.info("Adding offer {}", offerRequestDto);
+        OfferResponseDto savedOffer = offerFacade.saveOffer(offerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedOffer);
     }
 }
